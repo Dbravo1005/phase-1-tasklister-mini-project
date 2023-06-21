c971e6866e8457b34247732b9644245b1355899a
Damian Bravo <Dbravo1005@gmail.com>
2023-06-20 21:42:10 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const inputValue = document.getElementById('new-task-description').value
+    handle(inputValue);
+    form.reset();
+  })
 });
+
+function handle(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deletion)
+  btn.textContent = 'x' 
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+};
+
+function deletion(e){
+  e.target.parentNode.remove()
+};
+
